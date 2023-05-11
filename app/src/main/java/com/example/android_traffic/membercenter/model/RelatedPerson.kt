7756612940d666d691eca80641f9fd72420d258d
unnamed <x1593572582004@yahoo.com.tw>
2023-05-11 19:50:36 +0800

    漏傳

---
+++ app/src/main/java/com/example/android_traffic/membercenter/model/RelatedPerson.kt
@@ -0,0 +1,19 @@
+package com.example.android_traffic.membercenter.model
+
+import java.io.Serializable
+import java.util.Date
+
+/**
+ *
+ * @param imageId 頭像
+ * @param name 名子
+ * @param identityNumber 身分證
+ * @param birthday 生日日
+ * @param MembersRelationship 跟會員的關係
+ * @param VehideType 車種
+ * @param VehideNumber 車牌號碼
+ */
+class RelatedPerson(var imageId: Int, var name: String, var identityNumber: String, var birthday: Date,
+                    var MembersRelationship: String, var VehideType: String, var VehideNumber:String) :
+    Serializable {
+}
