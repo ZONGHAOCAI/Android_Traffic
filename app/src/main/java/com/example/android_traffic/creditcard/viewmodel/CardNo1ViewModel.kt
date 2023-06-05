74b40cf4468c46b4d3bb78bd11f70c3eb4ba5e35
KJ <klovefly520@gmail.com>
2023-06-05 17:03:59 +0800

    新增罰單申訴流程,信用卡新增介面

---
+++ app/src/main/java/com/example/android_traffic/creditcard/viewmodel/CardNo1ViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.android_traffic.creditcard.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class CardNo1ViewModel : ViewModel() {
+
+}
--- app/src/main/java/com/example/android_traffic/ticket/controller/TicketContentFragment.kt
+++ app/src/main/java/com/example/android_traffic/ticket/controller/TicketContentFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
 import com.example.android_traffic.R
 import com.example.android_traffic.databinding.FragmentTicketContentBinding
 import com.example.android_traffic.ticket.model.Content
@@ -28,10 +29,17 @@ class TicketContentFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        with(binding){
+            btTicketContentAppeal.setOnClickListener{
+Navigation.findNavController(it).navigate(R.id.ticketAppealtext2Fragment)
+            }
+        }
+
         arguments?.let {
             it.getSerializable("number")?.let {
                 binding.viewmodel?.content?.value = it as Content
             }
         }
+
     }
 }
