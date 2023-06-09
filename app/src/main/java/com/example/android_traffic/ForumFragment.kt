45e304f00f1cf01f5512fea6d2f7da7e9ac179ab
Eric <eric98765445123@gmail.com>
2023-06-09 14:07:27 +0800

    修改論壇

--- app/src/main/java/com/example/android_traffic/ForumFragment.kt
+++ app/src/main/java/com/example/android_traffic/ForumFragment.kt
@@ -6,26 +6,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.android_traffic.databinding.FragmentForumBinding
 
 class ForumFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ForumFragment()
-    }
-
-    private lateinit var viewModel: ForumViewModel
+    private lateinit var binding: FragmentForumBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_forum, container, false)
+    ): View {
+        binding  = FragmentForumBinding.inflate(inflater, container, false)
+        val viewModel : ForumViewModel by viewModels()
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ForumViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        with(binding){
+            rvForumArticleList.layoutManager = LinearLayoutManager(requireContext())
+            viewModel?.rvArticleList?.observe(viewLifecycleOwner){
+                if (rvForumArticleList.adapter == null)
+                    rvForumArticleList.adapter = ForumAdapter(it)
+            }
+        }
     }
+
+
 
 }
